b05f6a298bea942958d18526f4a80f76076b316b
Titouan-Joseph REVOL <titouanjoseph@gmail.com>
2020-07-17 18:51:35 +0200

    end of newyear command

--- cogs/newyear.py
+++ cogs/newyear.py
@@ -13,5 +13,25 @@ class CogNewyear(commands.Cog):
 
     @commands.command(aliases=["ny"])
     async def newyear(self, ctx):
-        await ctx.send("ok, nouvelle annee")
+        for member in ctx.guild.members:
+            if MyUtils(ctx.guild).getFuturTCRole() in member.roles:
+                await member.remove_roles(MyUtils(ctx.guild).getFuturTCRole())
+                await member.add_roles(MyUtils(ctx.guild).get3TCRole())
+            elif MyUtils(ctx.guild).get3TCRole() in member.roles:
+                await member.remove_roles(MyUtils(ctx.guild).get3TCRole())
+                await member.add_roles(MyUtils(ctx.guild).get4TCRole())
+            elif MyUtils(ctx.guild).get4TCRole() in member.roles:
+                await member.remove_roles(MyUtils(ctx.guild).get4TCRole())
+                await member.add_roles(MyUtils(ctx.guild).get5TCRole())
+            elif MyUtils(ctx.guild).get5TCRole() in member.roles:
+                await member.remove_roles(MyUtils(ctx.guild).get5TCRole(),
+                                          MyUtils(ctx.guild).getStudentRole())
+                await member.add_roles(MyUtils(ctx.guild).getDiplomesRole())
+
+        await ctx.send("Changement des rôles :) : \n "
+                                   " - les Futur sont maintenant des 3TC \n"
+                                   " - les 3TC sont maintenant des 4TC \n"
+                                   " - les 4TC sont maintenant des 5TC \n"
+                                   " - les 5TC sont maintenant des Diplômés \n"
+                                   )
 
