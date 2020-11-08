0f2191093184181d84a213e412944518f155f1f4
Titouan-Joseph REVOL <titouanjoseph@gmail.com>
2020-11-08 23:33:52 +0100

    :sparkles: ajout d'un gateau :cake:

--- cogs/vendrediChill.py
+++ cogs/vendrediChill.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from myutils import MyUtils
 import random
+import time
 
 
 class CogVendrediChill(commands.Cog):
@@ -15,7 +16,7 @@ class CogVendrediChill(commands.Cog):
             await ctx.guild.create_text_channel(f"General", category=category)
             await ctx.guild.create_voice_channel(f"General", category=category)
             for i in range(15):
-                await ctx.guild.create_voice_channel(f"lobby-{i+1}", category=category)
+                await ctx.guild.create_voice_channel(f"lobby-{i + 1}", category=category)
 
     @commands.command()
     async def hideChannel(self, ctx):
@@ -38,7 +39,7 @@ class CogVendrediChill(commands.Cog):
             random.shuffle(conectedMenmbers)
             for member in conectedMenmbers:
                 print(member)
-                randomLobby = lobbyChan[random.randint(0, len(lobbyChan)-1)]
+                randomLobby = lobbyChan[random.randint(0, len(lobbyChan) - 1)]
                 print(randomLobby)
                 await member.move_to(randomLobby)
                 if len(randomLobby.members) == 5:
@@ -52,3 +53,31 @@ class CogVendrediChill(commands.Cog):
     async def beer(self, ctx):
         await ctx.send(":beers:")
 
+    @commands.command()
+    async def cookie(self, ctx):
+        await ctx.send("https://img2.freepng.fr/20180701/yyq/kisspng-chocolate-chip-cookie-cookie-monster-macaron"
+                       "-biscu-crazy-cartoon-5b38b0743f46d3.7361440815304418442592.jpg")
+
+    @commands.command(aliases=["gateau"])
+    async def cake(self, ctx):
+        count = 0
+
+        def checkMessage(message):
+            return message.channel == ctx.message.channel
+
+        msg = await ctx.send(":birthday:" * 3)
+        try:
+            while count < 5:
+                soufle = await self.bot.wait_for("message", timeout=15, check=checkMessage)
+                if soufle.content == "🌬️":
+                    count += 1
+            await msg.edit(content=":moon_cake:" * 3)
+        except:
+            if count == 0:
+                await ctx.send("Personne n'a souflé... \n "
+                               "Le gateau a pris feu :fire:")
+            else:
+                await ctx.send("Vous n'avez pas assez souflé... \n "
+                               "Le gateau a pris feu :fire:")
+            await msg.edit(content=":fire:" * 3)
+            return
