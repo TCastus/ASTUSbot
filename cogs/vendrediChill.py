4738a601758ba634489eed3f472197bbada8f2de
Titouan-Joseph REVOL <titouanjoseph@gmail.com>
2020-11-23 16:12:31 +0100

    fix(vendredichill): :heavy_plus_sign: add command for noe
    Changement du random pour cookie / brownie / beer
    Ajout d'une commande
    qui les display tous en meme temps

--- cogs/vendrediChill.py
+++ cogs/vendrediChill.py
@@ -13,6 +13,29 @@ def setup(bot):
 class CogVendrediChill(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.cookies = [
+            "https://tenor.com/view/cookie-monster-om-nom-omnomnom-sesame-gif-10644609",
+            "https://img2.freepng.fr/20180701/yyq/kisspng-chocolate-chip-cookie-cookie-monster-macaron-biscu"
+            "-crazy-cartoon-5b38b0743f46d3.7361440815304418442592.jpg",
+            "https://media.tenor.com/images/12fbaab498da0df08b4b7086954d5016/tenor.gif",
+            "https://media.tenor.com/images/626d54f8905f7fc267bb365f2aca4254/tenor.gif",
+            "https://media.tenor.com/images/53f9792c40f8c93e26b85c15bd28d175/tenor.gif",
+        ]
+        self.brownies = [
+            "https://i.pinimg.com/originals/b5/d3/44/b5d3445904e0fa9ffc0452f4a09afc5c.jpg",
+            "https://media.tenor.com/images/d99b1a30fc5e1fff78109930cab58ba9/tenor.gif",
+            "https://media.tenor.com/images/5910b399aad3a16a71faf11a1b1239a2/tenor.gif",
+            "https://media.tenor.com/images/bd022b60e0c69c4068eb47d7dfdc4edc/tenor.gif",
+        ]
+        self.beers = [
+            ":beers:",
+            ":beer:",
+            "https://media.tenor.com/images/218d4f5fd06af8ca4b2f9d7c524f1a59/tenor.gif",
+            "https://media.tenor.com/images/d44815a7735a0b7adc72c6bdba072fbc/tenor.gif",
+            "https://media.tenor.com/images/42b7583994f574f0a6521197d0454c33/tenor.gif",
+            "https://media.tenor.com/images/da89e4634ed028230dc5fb0a5a112174/tenor.gif",
+            "https://media.tenor.com/images/3b07b01b63abbdd75f3c54bce3841212/tenor.gif",
+        ]
 
     @commands.command()
     async def showChannel(self, ctx, number=15):
@@ -67,40 +90,24 @@ class CogVendrediChill(commands.Cog):
     @commands.command()
     async def brownie(self, ctx):
         await self.bot.change_presence(activity=discord.Game(name="mange une part de brownie"))
-        brownies = [
-            "https://i.pinimg.com/originals/b5/d3/44/b5d3445904e0fa9ffc0452f4a09afc5c.jpg",
-            "https://media.tenor.com/images/d99b1a30fc5e1fff78109930cab58ba9/tenor.gif",
-            "https://media.tenor.com/images/5910b399aad3a16a71faf11a1b1239a2/tenor.gif",
-            "https://media.tenor.com/images/bd022b60e0c69c4068eb47d7dfdc4edc/tenor.gif",
-        ]
-        await ctx.send(brownies[random.randint(0, len(brownies) - 1)])
+        await ctx.send(random.choice(self.brownies))
 
     @commands.command(aliases=["biere"])
     async def beer(self, ctx):
         await self.bot.change_presence(activity=discord.Game(name="Boit une bonne biere"))
-        beers = [
-            ":beers:",
-            ":beer:",
-            "https://media.tenor.com/images/218d4f5fd06af8ca4b2f9d7c524f1a59/tenor.gif",
-            "https://media.tenor.com/images/d44815a7735a0b7adc72c6bdba072fbc/tenor.gif",
-            "https://media.tenor.com/images/42b7583994f574f0a6521197d0454c33/tenor.gif",
-            "https://media.tenor.com/images/da89e4634ed028230dc5fb0a5a112174/tenor.gif",
-            "https://media.tenor.com/images/3b07b01b63abbdd75f3c54bce3841212/tenor.gif",
-        ]
-        await ctx.send(beers[random.randint(0, len(beers) - 1)])
+        await ctx.send(random.choice(self.beers))
 
     @commands.command()
     async def cookie(self, ctx):
         await self.bot.change_presence(activity=discord.Game(name="Mange un cookie"))
-        cookies = [
-            "https://tenor.com/view/cookie-monster-om-nom-omnomnom-sesame-gif-10644609",
-            "https://img2.freepng.fr/20180701/yyq/kisspng-chocolate-chip-cookie-cookie-monster-macaron-biscu"
-            "-crazy-cartoon-5b38b0743f46d3.7361440815304418442592.jpg",
-            "https://media.tenor.com/images/12fbaab498da0df08b4b7086954d5016/tenor.gif",
-            "https://media.tenor.com/images/626d54f8905f7fc267bb365f2aca4254/tenor.gif",
-            "https://media.tenor.com/images/53f9792c40f8c93e26b85c15bd28d175/tenor.gif",
-        ]
-        await ctx.send(cookies[random.randint(0, len(cookies) - 1)])
+        await ctx.send(random.choice(self.cookies))
+
+    @commands.command(aliases=["beerscookiebrownies", "cookiebeerbrownies"])
+    async def browniesbeercookie(self, ctx):
+        await self.bot.change_presence(activity=discord.Game(name="Se fait un bon goûter"))
+        await ctx.send(random.choice(self.beers))
+        await ctx.send(random.choice(self.brownies))
+        await ctx.send(random.choice(self.cookies))
 
     @commands.command(aliases=["gateau"])
     async def cake(self, ctx, number=3):
