e36fffce60fed58884d70136859fe6aacda309ed
Titouan-Joseph REVOL <titouanjoseph@gmail.com>
2020-12-27 00:32:25 +0100

    feat(memes): :star: add meme command
    Add database in json file
    Add cog memes
    Update main for load cog

--- main.py
+++ main.py
@@ -278,5 +278,6 @@ if __name__ == '__main__':
     bot.load_extension("cogs.pot_tres_confine")
     bot.load_extension("cogs.tgg.cog")
     bot.load_extension("cogs.bastos")
+    bot.load_extension("cogs.memes.cog_meme")
 
     bot.run(TOKEN)
